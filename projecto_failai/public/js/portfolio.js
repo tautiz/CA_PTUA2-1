8254dd7f3ad60f40e654716f531ef25c50540652
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-05-04 20:18:53 +0300

    Intro to Dialogs in HTML and Event listeners in JS

---
+++ projecto_failai/public/js/portfolio.js
@@ -0,0 +1,48 @@
+window.onload = () => {
+    const darbai = document.getElementsByClassName('darbas');
+    const favDialog = document.getElementById('favDialog');
+    const closeBtn = favDialog.querySelector('dialog > [title="Close"]');
+
+    // Dialogo uždaryui mygtuko "Close" įvykio registravimas
+    closeBtn.addEventListener('click', closeModal);
+
+    // Modalo uždarymas kai paspaudžiama už jo ribų.
+    favDialog.addEventListener("click", e => {
+        const dialogDimensions = favDialog.getBoundingClientRect()
+        if (
+            e.clientX < dialogDimensions.left ||
+            e.clientX > dialogDimensions.right ||
+            e.clientY < dialogDimensions.top ||
+            e.clientY > dialogDimensions.bottom
+        ) {
+            favDialog.close()
+        }
+    })
+
+    // Su kiekvienu galerijos darbo elementu sukamas ciklas ir jam regisruojamas įvykio klausytojas
+    for( let darbas of darbai) {
+        darbas.addEventListener('click', loadImageAndShowDModal);
+    }
+
+    function loadImageAndShowDModal(event) {
+        // Čia nusprendžiame iš kur imsime padidintą foto variantą
+
+        // Iš Evento išraukiame elementą ant kurio katik paspaudėme
+        // let element = event.target;
+        // let imagePath = element.getAttribute('src');
+
+        // Ar tiesiog nurodome foto adresą kaip konstantą
+        let imagePath = 'https://picsum.photos/400/600';
+
+        let image = favDialog.querySelector('img');
+
+        image.src = imagePath; // Pakeičiame paveikslelio kelia į naują
+
+        favDialog.showModal(); // Dialogo parodymas
+    }
+
+    function closeModal(event) {
+        event.preventDefault(); // Nes mes nenorime išsiūsti duomenų su galima forma
+        favDialog.close(); // Dialogo uždarymas.
+    }
+}
