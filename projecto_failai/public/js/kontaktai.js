f2f88f95a843d3b93ad8b310dce9f62adb166a3e
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-05-08 20:55:47 +0300

    Intro to JS Forms and form submition events

--- projecto_failai/public/js/kontaktai.js
+++ projecto_failai/public/js/kontaktai.js
@@ -1,8 +1,11 @@
 const manoForma = document.querySelector('form');
+const output = document.getElementById('output');
 
 manoForma.addEventListener('submit', function (e) {
     e.preventDefault();
     let vardas = e.target.elements.vardas.value;
-    // ??????
-    console.log(vardas)
+    let pavarde = e.target.elements.pavarde.value;
+
+    output.style.margin = '5px';
+    output.innerHTML = `Vardas: ${vardas} <br>Pavardė: ${pavarde}`;
 })
